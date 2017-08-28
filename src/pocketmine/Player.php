e4be2110bee7ff8d01ee983834fcec874f040363
Kroer Max <kroer.m.d@gmail.com>
2017-08-28 16:03:30 +0600

    fixed logic for non supporting versions disconnecting

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1740,6 +1740,7 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer{
 					break;
 				}
 				if($packet->isValidProtocol === false) {
+					$this->protocol = $packet->protocol1; // we need protocol for correct encoding DisconnectPacket
 					$this->close("", TextFormat::RED . "Please switch to Minecraft: PE " . TextFormat::GREEN . $this->getServer()->getVersion() . TextFormat::RED . " to join.");
 					//Timings::$timerLoginPacket->stopTiming();
 					break;
